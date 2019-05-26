f98b71784c21699bb2eeaa7dab0b70e451e33fe7
BlueWolfDZ <46226823+BlueWolfDZ@users.noreply.github.com>
2019-05-26 07:37:53 +0100

    Add files via upload

---
+++ system.js
@@ -0,0 +1,5 @@
+exports.TOKEN = 'process.env.LIGHT'; // التوكن
+exports.prefix = '.'; // بادئة البوت
+
+exports.LIMIT = 1000; // اقصى عدد لمسح الرسائل
+exports.COUNT = 50; // اقصى عدد لمسح الرسائل بدون تحديد قيمة
