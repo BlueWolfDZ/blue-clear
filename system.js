3adb3a4766b6aca06a0ed9364c44cc935dba56be
BlueWolfDZ <46226823+BlueWolfDZ@users.noreply.github.com>
2019-05-26 07:50:48 +0100

    Delete system.js

--- system.js
+++
@@ -1,5 +0,0 @@
-exports.TOKEN = 'process.env.LIGHT'; // التوكن
-exports.prefix = '.'; // بادئة البوت
-
-exports.LIMIT = 1000; // اقصى عدد لمسح الرسائل
-exports.COUNT = 50; // اقصى عدد لمسح الرسائل بدون تحديد قيمة
